509a91ced697c99e7b18fd53cfb519f23d11aa55
divyanshumishra513-ai <divyanshumishra513@gmail.com>
2026-03-03 22:44:49 +0530

    uc3.java
    uc3.java

---
+++ uc3.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp
+ * 
+ * This application prints "OOPS" (Object-Oriented Programming System)
+ * to the console.
+ * 
+ * @author Divyanshu
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the Java application
+     * 
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Printing OOPS to console
+        System.out.println("OOPS");
+    }
+}
